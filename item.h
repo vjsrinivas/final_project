5f8274736c8164854149b48c832bc49d806b105f
John Pi <jpi@hydra15.eecs.utk.edu>
2019-04-01 13:55:05 -0400

    initial

---
+++ item.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+class Item{
+	Public:
+	Item();
+	~Item();
+
+
+	Private:
+	string Name;
+	int damage;
+	int health;
+	int location;
+
+}
---
+++ monster.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Monster{
+	Public:
+	Monster();
+	~Monster();
+
+	Private:
+	string name;
+	int health;
+	int damage;
+	int speed;
+	int position;		   
+}
+
