e05da6a97be630d0d64df66a600e1e4bd7e309a3
John Pi <jpi@hydra15.eecs.utk.edu>
2019-04-01 15:56:29 -0400

    initial

---
+++ monster.cpp
@@ -0,0 +1,35 @@
+#include "monster.h"
+
+using namespace std;
+
+class Skeleton : public Monster{
+	public:
+		Skeleton(){
+//			this->name="Skeleton";
+			this->health=1;
+			this->damage=1;
+			this->speed=1;
+//			this->position= ; 
+		}
+		void attack(){
+			if(){//if player character is 1 block away
+				//player health - this->monster_damage 	
+			}
+		}
+}
+
+class Zombie : public Monaster{
+	public:
+		Zombie(){
+//			this->name="Zombie"
+			this->health=2;
+			this->damage=2;
+			this->speed=1;
+//			this->position= ; 
+		}
+		void attack(){
+			if(){
+			
+			}
+		}
+}
