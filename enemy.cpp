d6ec17f2e7c91205b16fafed2bad1003505bebb8
Vijaysrinivas Rajagopal <vrajago2@arc15.eecs.utk.edu>
2019-04-24 19:10:49 -0400

    added enemy stuff

--- enemy.cpp
+++ enemy.cpp
@@ -1,6 +1,3 @@
-#include <cstdio>
-#include <string>
-//#include "player.h"
 #include "enemy.h"
 
 void Enemy::printPos(){
@@ -24,7 +21,7 @@ Enemy::Enemy(std::string path){
 //}
 
 void loadEnemyFile(std::string filename, std::vector<Enemy*>& enemy){
-	ifstream file(filename.c_str());
+	std::ifstream file(filename.c_str());
 	
 	if(file.is_open()){
 		int numofenemies;
@@ -36,6 +33,7 @@ void loadEnemyFile(std::string filename, std::vector<Enemy*>& enemy){
 			Enemy* new_enemy = new Enemy();
 			file >> new_enemy->name;
 			file >> new_enemy->texturePath;
+			enemy.push_back(new_enemy);
 		}
 	}
 	else{
--- enemy.h
+++ enemy.h
@@ -1,7 +1,10 @@
 #include <cstdio>
 #include <string>
+#include <vector>
+#include <fstream>
 #include "ltexture.h"
-//#include "player.h"
+#include "player.h"
+
 
 	
 class Enemy{
@@ -11,11 +14,13 @@ class Enemy{
 		void printPos();
 		Position pos;
 		int health=10;
-
+		std::string name;
+		std::string texturePath;
 //		void dead();
 //		void spawn();
 	private:
 //		void attack();
-}
+		
+};
 
 void loadEnemyFile(std::string, std::vector<Enemy*>&);
