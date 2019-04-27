2638e663b40fac91e68c1df432956bf0df1ba4b6
Joshua Clark Spangler <jspangl3@arc15.eecs.utk.edu>
2019-04-27 09:15:23 -0400

    map.h and map.cpp commented

--- map.cpp
+++ map.cpp
@@ -6,6 +6,7 @@
 		terrain = terr;
 	}
 
+	//reads in a file that will create the map
 	Map::Map(int x, int y, int radius, std::string filename, SDL_Renderer* gRenderer, std::vector<Item*> items, std::vector<Enemy*> enemies){
 		std::ifstream file_in(filename.c_str());
 		int map_x, map_y;
@@ -15,7 +16,7 @@
 			//read in texture paths:
 			int texture_id = 0;
 			std::string texture_path;
-			
+
 			while(file_in >> texture_id){
 				if(texture_id != -1){
 					file_in >> texture_path;
@@ -23,16 +24,16 @@
 				}
 				else
 					break;
-			}
-			
+			}//checks the terrain type and saves the path to the texture for rendering
+
 			int map_buffer;
 			file_in >> map_x >> map_y;
 			width = map_x;
 			height = map_y;
-			
+
 			int tmp_x = 0;
 			int tmp_y = 0;
-			
+
 			//resize vector:
 			for(int i=0; i < map_y; i++){
 				std::vector<Node*> row;
@@ -50,14 +51,14 @@
 					std::cout << map_buffer << " ";
 				}
 				std::cout << std::endl;
-			}	
+			}
 
 			file_in >> map_buffer;
 
 			while(file_in >> map_buffer){
 				//std::cout << map_buffer << std::endl;
 				if(map_buffer != -1){
-					int coor_x = 0; 
+					int coor_x = 0;
 					int coor_y = 0;
 					file_in >> coor_x >> coor_y;
 					std::cout << "id: " << map_buffer << " " << coor_x << " " << coor_y << std::endl;
@@ -81,8 +82,8 @@
 				}
 				else
 					break;
-			}
-			
+			}//creates the enemies and figures out their locations on the map
+
 			std::cout << std::endl;
 			for(int i=0; i < node_map.size(); i++){
 				for(int j=0; j < node_map[i].size(); j++){
@@ -92,34 +93,34 @@
 						std::cout << "0 ";
 				}
 				std::cout << std::endl;
-			}
+			}//prints a matrix of the map checking which spaces are occupied by an enemy/weapon/player
 	}
 	else{
-		printf("Error: file is not opened\n");	
+		printf("Error: file is not opened\n");
 	}
 
 	//load in shimmer texture:
 	render = gRenderer;
 	shimmer->loadFromFile(render, "./assets/shimmer.png");
-	loadtextures(x,y,radius);
+	loadtextures(x,y,radius);//renders the shimmer that indicates an item
 }
 
 void Map::RemoveItem(int y, int x){
 	node_map[y][x]->currItem = NULL;
-}
+}//takes an item off the map
 
 Map::Map(int width, int height, SDL_Renderer* gRenderer){
 	render = gRenderer;
 
 	LoadMap(width, height);
-}
+}//calls the renderer for the map
 
 void Map::LoadMap(int width, int height){
 	if(width > 0 && height > 0){
      for(int i=0; i < height; i++){
 			std::vector<Node*> width_v;
 			for(int j=0; j < width; j++){
-				Node* default_value = new Node(0); 
+				Node* default_value = new Node(0);
 				width_v.push_back(default_value);
 			}
 			node_map.push_back(width_v);
@@ -146,7 +147,7 @@ void Map::loadtextures(){
 			dirt->render(render, j*30, i*30);
 		}
 	}
-}
+}//renders a "blank" map of dirt if passed no variables
 
 void Map::loadtextures(int x, int y, int radius){
 		for(std::map<int,std::string>::iterator it=terrain_key.begin(); it != terrain_key.end(); it++){
@@ -160,7 +161,7 @@ void Map::loadtextures(int x, int y, int radius){
 		}
 
 		std::cout << "node size: " << node_map.size() << " " << node_map[0].size() << std::endl;
-		
+
 		//i <- y
 		//j <- x
 		for(int i=0; i < node_map.size(); i++){
@@ -175,8 +176,8 @@ void Map::loadtextures(int x, int y, int radius){
 					texture->render(render,j*30,i*30);
 				}
 			}
-		}		
-}
+		}
+}//renders the correct map textures when given the proper parameters
 
 void Map::ClearMap(){
 
@@ -184,14 +185,14 @@ void Map::ClearMap(){
 
 Node* Map::GetNode(int x, int y){
 	return node_map[x][y];
-}
+}//returns a particular node on the map
 
 void Map::Redraw(int x, int y, int radius){
 	//compute which blocks to render:
 	std::vector<Position> torender;
 	Position original; original.x = 0; original.y = 0;
 	torender.resize((2*radius+1)*(2*radius+1), original);
-	
+
 	int step_x = -1*(radius);
 	int step_y = -1*(radius);
 
@@ -218,14 +219,14 @@ void Map::Redraw(int x, int y, int radius){
 		LTexture* texture = textures[textures.size()-1];
 		texture->render(render, (step_x-1)*30,(step_y-1)*30);
 
-		if(node_map[step_y-1][step_x-1]->currItem != NULL){	
+		if(node_map[step_y-1][step_x-1]->currItem != NULL){
 			shimmer->render(render, (step_x-1)*30, (step_y-1)*30);
 		}
 	}
 
 	for(int i=0; i < torender.size(); i++){
 		Position resultpos;
-		resultpos.x = x+torender[i].x; 
+		resultpos.x = x+torender[i].x;
 		resultpos.y = y+torender[i].y;
 		if(resultpos.x >= 0 && resultpos.y >= 0 && resultpos.x < node_map[0].size() && resultpos.y < node_map.size()){
 			//std::cout << resultpos.x << " " << resultpos.y << std::endl;
@@ -245,7 +246,8 @@ void Map::Redraw(int x, int y, int radius){
 			}
 		}
 	}
-}
+}//since our game only shows you a certain sized area at a time, this function renders a particular area around the player, determined by the given parameters
+
 
 
 Dot::Dot()
@@ -269,7 +271,7 @@ Dot::Dot(const int SCREEN_WIDTH, const int SCREEN_HEIGHT, std::string texturePat
 	//Initialize the velocity
 	mVelX = 0;
 	mVelY = 0;
- 
+
 	controller = new Player(texturePath);
 
   //set screen size:
@@ -294,7 +296,7 @@ void Dot::handleEvent( SDL_Event& e)
 	}*/
 	//If a key was released
 	//else if( e.type == SDL_KEYUP && e.key.repeat == 0 )
-	
+
 	if(e.type == SDL_KEYUP && e.key.repeat == 0)
 	{
 		//controller->movesLeft--;
@@ -364,7 +366,7 @@ void Dot::move(Map*& map)
 	//printf("mposx: %i | mposy: %i | mvelx: %i | mvely: %i | dotheight: %i | screenheight %i\n", mPosX, mPosY, mVelX, mVelY, DOT_HEIGHT, SCREEN_HEIGHT);
 	//controller->printPos();
 	//printf("Real location is: %i %i\n", mPosX, mPosY);
-	
+
 	// Item detection:
 	// maybe move to seperate function:
 	if(map->GetNode(controller->pos.y, controller->pos.x)->currItem != NULL){
@@ -372,17 +374,17 @@ void Dot::move(Map*& map)
 		printf("item picked up!\n");
 	}
 
-	
+
 	mVelX = 0;
 	mVelY = 0;
-}
+}//handles the player running into iteams or walls
 
 //add to player's inventory, use if potion
 void Dot::itemPickup(Map*& map, int y, int x){
 	Item* item = map->GetNode(y,x)->currItem;
 	executeItem(item, controller);
 	controller->addItem(item);
-	
+
 	// TEMP:
 	if(item->type == "weapon")
 		controller->currWeap = item;
@@ -393,6 +395,6 @@ void Dot::itemPickup(Map*& map, int y, int x){
 
 void Dot::render(LTexture& gDotTexture, SDL_Renderer* gRenderer)
 {
-	//Show the dot
+	//Show the player
 	gDotTexture.render(gRenderer, mPosX, mPosY );
 }
